641eaccdf6929e56f212e1fe7e705134dc140346
rosd89 <zkdlffpem@naver.com>
2017-07-01 18:28:56 +0900

    [create] 'NaHun.vue' Component 파일 생성
    메인 페이지로 지정

---
+++ step2/src/components/NaHun.vue
@@ -0,0 +1,14 @@
+<!--
+ * Created by sam on 2017. 7. 1..
+-->
+<template>
+  <div>
+    <img src="../assets/na-hun.jpg"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NaHun'
+  }
+</script>
