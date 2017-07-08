3b41c2aeff97820a52683878e1cbba9ba2af9524
rosd89 <zkdlffpem@naver.com>
2017-07-09 04:25:25 +0900

    [create] Bottom 구현

--- step3/src/App.vue
+++ step3/src/App.vue
@@ -2,13 +2,17 @@
   <div class="wrapper">
     <top-section></top-section>
     <router-view></router-view>
+    <bottom-section></bottom-section>
   </div>
 </template>
 
 <script>
   import TopSection from './components/layout/TopSection'
+  import BottomSection from './components/layout/BottomSection'
   export default {
-    components: {TopSection},
+    components: {
+      TopSection, BottomSection
+    },
     name: 'app'
   }
 </script>
--- step3/src/components/layout/TopSection.vue
+++ step3/src/components/layout/TopSection.vue
@@ -3,10 +3,13 @@
 -->
 <template>
   <header class="main-header">
-    <nav class="navbar navbar-static-top">
+    <nav class="navbar navbar-static-top nav-background-color">
       <div class="container">
         <div class="navbar-header">
-          <a href="#" class="navbar-brand"><b>Admin</b>LTE</a>
+          <a href="#" class="navbar-brand">
+            <img class="logo-s" src="/static/img/logo-s.png"/>
+            <b>Twice</b>
+          </a>
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
             <i class="fa fa-bars"></i>
           </button>
@@ -21,3 +24,20 @@
     name: 'TopSection'
   }
 </script>
+
+<style>
+  .nav-background-color {
+    background-color: #333;
+  }
+
+  .navbar-brand > img {
+    display: inline;
+  }
+
+  .logo-s {
+    width: 104px;
+    height: 40px;
+    margin-top: -15px;
+    margin-right: 10px;
+  }
+</style>
